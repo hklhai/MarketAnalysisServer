e219ff7cce3df65fc74635150580f780beab0344
hklhai <hkhai@outlook.com>
2018-09-13 14:36:48 +0800

    增加查询爬取数据接口

--- src/main/java/com/hxqh/ma/controller/ShowController.java
+++ src/main/java/com/hxqh/ma/controller/ShowController.java
@@ -1,9 +1,16 @@
 package com.hxqh.ma.controller;
 
+import com.hxqh.ma.model.assist.StatusDto;
 import com.hxqh.ma.service.SystemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Ocean lin on 2017/7/1.
@@ -78,6 +85,28 @@ public class ShowController {
     @RequestMapping("/book")
     public String book() {
         return "show/book";
+    }
+
+
+    @RequestMapping("/status")
+    public String status() {
+        return "show/status";
+    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/statusData", method = RequestMethod.POST)
+    public StatusDto statusData(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                @RequestParam(value = "size", defaultValue = "15") Integer size) {
+        StatusDto listData = null;
+        Sort sort = new Sort(Sort.Direction.DESC, "sid");
+        try {
+            Pageable pageable = new PageRequest(page, size, sort);
+            listData = systemService.statusData(pageable);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return listData;
     }
 
 
